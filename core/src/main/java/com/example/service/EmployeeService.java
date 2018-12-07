ab03f1fae3f627a907fd4343d0986edf61ab311a
ravi kumar <91raviindia@gmail.com>
2018-12-07 23:44:02 +0530

    using scope

--- core/src/main/java/com/example/service/EmployeeService.java
+++ core/src/main/java/com/example/service/EmployeeService.java
@@ -2,16 +2,18 @@ package com.example.service;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
 @Service
+@Scope(ConfigurableBeanFactory.SCOPE_SINGLETON)
 public class EmployeeService {
-	
+
 	public static Logger logger = LoggerFactory.getLogger(EmployeeService.class);
 
-
 	public void getEnploye() {
-		
+
 		logger.info("employee");
 	}
 
