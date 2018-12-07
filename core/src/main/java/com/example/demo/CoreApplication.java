4b43989268a03f98a88c4f263d37a98e5d79cee6
ravi kumar <91raviindia@gmail.com>
2018-12-07 22:21:49 +0530

    adding EmployeeService class and getting from container

--- core/src/main/java/com/example/demo/CoreApplication.java
+++ core/src/main/java/com/example/demo/CoreApplication.java
@@ -2,11 +2,23 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class CoreApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CoreApplication.class, args);
+		ConfigurableApplicationContext ctx = SpringApplication.run(CoreApplication.class, args);
+
+		String[] beanDefinitionNames = ctx.getBeanDefinitionNames();
+		for (String beanDefinitionName : beanDefinitionNames) {
+			System.out.println("beanDefinitionNames :: " + beanDefinitionName);
+		}
+
+		System.out.println("count :: " + ctx.getBeanDefinitionCount());
+
+		EmployeeService employeeService = ctx.getBean(EmployeeService.class);
+		employeeService.getEnploye();
+
 	}
 }
